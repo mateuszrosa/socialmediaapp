400eff3ad389dff80bfeba6da3e9e7e384a02cc9
mateuszrosa <mateusz.rosa@gmail.com>
2020-09-07 12:55:56 +0200

    styled form

--- src/Login.js
+++ src/Login.js
@@ -7,11 +7,12 @@ const Login = () => {
       <div className={styles.login}>
         <h1>Just Log In</h1>
         <form action="">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor="login">Login</label>
+          <input type="text" name="login" id="login" />
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
-          <input type="submit" value="LogIn" />
+          <input type="submit" value="Log In" />
+          <input type="button" value="Register" />
         </form>
       </div>
     </div>
