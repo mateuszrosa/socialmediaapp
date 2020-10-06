3758c3b302642ac85e069bf28bc89dc1475c37bc
mateuszrosa <mateusz.rosa@gmail.com>
2020-10-06 12:15:22 +0200

    validation for sending post

--- src/components/NewPostBar/NewPostBar.js
+++ src/components/NewPostBar/NewPostBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from "react-redux";
 import styles from 'components/NewPostBar/NewPostBar.module.scss';
 import close from 'assets/close-fat.svg';
@@ -7,28 +7,37 @@ import {addPost as addPostAuth} from 'actions'
 
 const NewPostBar = ({hideBar, addPost}) => {
 
+    const [valid, setValid] = useState(false) 
+
     const formik = useFormik({
         initialValues: {
             text: "",
         },
 
     onSubmit: ({text}) => {
+        if(text) {
         addPost(text)
         hideBar()
+        } else {
+            setValid(true)
+        }
+
     }
     });
     return ( 
         <div className={styles.bar}>
             <img onClick={hideBar} src={close} alt=""/>
-            <h1>Create Post</h1>    
+            <h1>Create Post</h1>
+            {valid && <p>You can't send empty post!</p>}
             <form onSubmit={formik.handleSubmit}>
                 <textarea 
-            onChange={formik.handleChange}
-            value={formik.values.text}
-            name="text" 
-            id="text" 
-            cols="25" 
-            rows="5"></textarea>
+                    onChange={formik.handleChange}
+                    value={formik.values.text}
+                    name="text" 
+                    id="text" 
+                    cols="25" 
+                    rows="5">
+                </textarea>
             <input type="submit" value="Post"/>
             </form>
         </div>
