7f3f3c5abc0958270a86a1393b2e11e178ba80cb
mateuszrosa <mateusz.rosa@gmail.com>
2020-10-14 16:35:22 +0200

    searching users

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchUserProfile} from 'actions'
 import "./Header.module.scss";
+import { useFormik } from "formik";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
+
+  const {posts} = useSelector(state => ({
+    posts: state.posts
+  }))
+
+  const dispatch = useDispatch();
+  const [nicks, setNicks] = useState([]);
+
+  const change = (e) => {
+    let arr = [];
+    if(e.target.value === "") {
+      arr = [];
+    } else {
+      for (const post of posts) {
+        if(post.login.startsWith(e.target.value)) {
+          !arr.includes(post.login) && arr.push(post.login)
+        }
+      }
+    }
+    setNicks(arr)
+  }
+
   return (
     <header>
-      <form action="">
+      <form>
         <input
+          onKeyUp={change}
           type="search"
-          name="search"
-          id="search"
+          name="text"
+          id="text"
           placeholder="Write here"
         />
         <input type="submit" value="Search" />
+      <ul>
+          {nicks.map(nick => <li key={nick}>{nick}</li>)}
+      </ul>
       </form>
       <img src={logo} alt="" />
     </header>
