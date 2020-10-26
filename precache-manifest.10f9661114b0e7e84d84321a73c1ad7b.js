9d35f0d75aa79933fff9634f6e6cc9f01c1d4fe6
mateuszrosa <mateusz.rosa@gmail.com>
2020-10-26 18:01:59 +0100

    Updates

---
+++ precache-manifest.10f9661114b0e7e84d84321a73c1ad7b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "aca0b622b9e55fb609c8",
+    "url": "/socialmediaapp/static/css/main.4fceccd2.chunk.css"
+  },
+  {
+    "revision": "aca0b622b9e55fb609c8",
+    "url": "/socialmediaapp/static/js/main.aca0b622.chunk.js"
+  },
+  {
+    "revision": "2cd36344d6cea13bb674",
+    "url": "/socialmediaapp/static/js/1.2cd36344.chunk.js"
+  },
+  {
+    "revision": "3b28988c003e446eea82",
+    "url": "/socialmediaapp/static/js/runtime~main.3b28988c.js"
+  },
+  {
+    "revision": "c53369d502455737d2ef206a2e9cb4ec",
+    "url": "/socialmediaapp/static/media/male.c53369d5.svg"
+  },
+  {
+    "revision": "283e11ebb1320aac4c1f477a90fb3d94",
+    "url": "/socialmediaapp/static/media/close-fat.283e11eb.svg"
+  },
+  {
+    "revision": "5690b774afc5e5ec28146c95e5b4e6d8",
+    "url": "/socialmediaapp/static/media/message.5690b774.svg"
+  },
+  {
+    "revision": "79efc68df8464ab1c9de0d82f7e48e99",
+    "url": "/socialmediaapp/static/media/heart-thin.79efc68d.svg"
+  },
+  {
+    "revision": "10a8cd131d6ba81739c1a73afac0ca30",
+    "url": "/socialmediaapp/static/media/heart-black.10a8cd13.svg"
+  },
+  {
+    "revision": "23e1294f1b0d802179d22a60b13892e9",
+    "url": "/socialmediaapp/static/media/comment-blog.23e1294f.svg"
+  },
+  {
+    "revision": "eb81c70a890dea95c93ee78824155bf3",
+    "url": "/socialmediaapp/static/media/bin.eb81c70a.svg"
+  },
+  {
+    "revision": "63c7b0a63b76d809a6190eb11f8e6db9",
+    "url": "/socialmediaapp/static/media/expand.63c7b0a6.svg"
+  },
+  {
+    "revision": "45af15c675b0982208b7db653c8b1649",
+    "url": "/socialmediaapp/static/media/edit.45af15c6.svg"
+  },
+  {
+    "revision": "37032d4bc7cfa513a81b0d7ad2e792c3",
+    "url": "/socialmediaapp/static/media/add.37032d4b.svg"
+  },
+  {
+    "revision": "715999108ef2f448f16bef714fa75b8a",
+    "url": "/socialmediaapp/index.html"
+  }
+];
