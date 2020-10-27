96608611876585929d873e5ee9becc4d90e622e1
mateuszrosa <mateusz.rosa@gmail.com>
2020-10-27 10:47:00 +0100

    Updates

---
+++ precache-manifest.27ffd46abc230f720b7efa9abd70fb85.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "296d690acaced2052c9d",
+    "url": "/socialmediaapp/static/css/main.8c624f6c.chunk.css"
+  },
+  {
+    "revision": "296d690acaced2052c9d",
+    "url": "/socialmediaapp/static/js/main.296d690a.chunk.js"
+  },
+  {
+    "revision": "e74968f9a7b7d414a8eb",
+    "url": "/socialmediaapp/static/js/1.e74968f9.chunk.js"
+  },
+  {
+    "revision": "3b28988c003e446eea82",
+    "url": "/socialmediaapp/static/js/runtime~main.3b28988c.js"
+  },
+  {
+    "revision": "c53369d502455737d2ef206a2e9cb4ec",
+    "url": "/socialmediaapp/static/media/male.c53369d5.svg"
+  },
+  {
+    "revision": "283e11ebb1320aac4c1f477a90fb3d94",
+    "url": "/socialmediaapp/static/media/close-fat.283e11eb.svg"
+  },
+  {
+    "revision": "5690b774afc5e5ec28146c95e5b4e6d8",
+    "url": "/socialmediaapp/static/media/message.5690b774.svg"
+  },
+  {
+    "revision": "79efc68df8464ab1c9de0d82f7e48e99",
+    "url": "/socialmediaapp/static/media/heart-thin.79efc68d.svg"
+  },
+  {
+    "revision": "10a8cd131d6ba81739c1a73afac0ca30",
+    "url": "/socialmediaapp/static/media/heart-black.10a8cd13.svg"
+  },
+  {
+    "revision": "23e1294f1b0d802179d22a60b13892e9",
+    "url": "/socialmediaapp/static/media/comment-blog.23e1294f.svg"
+  },
+  {
+    "revision": "eb81c70a890dea95c93ee78824155bf3",
+    "url": "/socialmediaapp/static/media/bin.eb81c70a.svg"
+  },
+  {
+    "revision": "63c7b0a63b76d809a6190eb11f8e6db9",
+    "url": "/socialmediaapp/static/media/expand.63c7b0a6.svg"
+  },
+  {
+    "revision": "45af15c675b0982208b7db653c8b1649",
+    "url": "/socialmediaapp/static/media/edit.45af15c6.svg"
+  },
+  {
+    "revision": "37032d4bc7cfa513a81b0d7ad2e792c3",
+    "url": "/socialmediaapp/static/media/add.37032d4b.svg"
+  },
+  {
+    "revision": "d1930f719aa9081e3cae8a7c92da8a00",
+    "url": "/socialmediaapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://mateuszrosa.github.io/socialmediaapp/precache-manifest.205d0f480fe38bf1892928703f491b6a.js"
+  "/socialmediaapp/precache-manifest.27ffd46abc230f720b7efa9abd70fb85.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://mateuszrosa.github.io/socialmediaapp/index.html", {
+workbox.routing.registerNavigationRoute("/socialmediaapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
