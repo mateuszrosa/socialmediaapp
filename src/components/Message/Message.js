6c028f91a042a52fc9a05d28f6c506cab17df5b1
mateuszrosa <mateusz.rosa@gmail.com>
2020-10-28 11:24:06 +0100

    edited message

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
-import {useDispatch} from 'react-redux';
-import {deleteMessage} from 'actions';
-import {Link} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { deleteMessage } from 'actions';
+import { Link } from 'react-router-dom';
 import styles from './Message.module.scss';
 import close from 'assets/close-fat.svg';
 import message from 'assets/message.svg';
 
-const Message = ({id, sendMessage, senderId, senderName, date, text, to, sent}) => {
+const Message = ({ id, sendMessage, senderId, senderName, date, text, to, sent }) => {
 
 
     const dispatch = useDispatch()
     const remove = () => {
         let box;
-        if(sent) {
-            dispatch(deleteMessage(id, senderName, box="sent"))
+        if (sent) {
+            dispatch(deleteMessage(id, senderName, box = "sent"))
         } else {
-            dispatch(deleteMessage(id, to, box="inbox"))
+            dispatch(deleteMessage(id, to, box = "inbox"))
         }
     }
 
-    return ( 
-    <div className={styles.message}>
-        <ReactTooltip />
-        <img data-tip="remove message" onClick={remove} src={close} alt="" />
-        {!sent && <img data-tip="respond" onClick={() => sendMessage(senderName)} src={message} alt="" />}
-        <div className={styles.image}></div>
-        <div className={styles.text}>
-            <h3>From: <Link to={`profile/${senderId}`}>{senderName}</Link></h3>
-            <span>{date}</span>
-            <p>{text}</p>
-        </div>
-    </div> );
+    return (
+        <div className={styles.message}>
+            <ReactTooltip />
+            <img data-tip="remove message" onClick={remove} src={close} alt="" />
+            {!sent && <img data-tip="respond" onClick={() => sendMessage(senderName)} src={message} alt="" />}
+            <div className={styles.image}></div>
+            <div className={styles.text}>
+                {sent
+                    ?
+                    <h3>To: <span>{to}</span></h3>
+                    :
+                    <h3>From: <Link to={`profile/${senderId}`}>{senderName}</Link></h3>}
+                <span>{date}</span>
+                <p>{text}</p>
+            </div>
+        </div>);
 }
- 
+
 export default Message;
