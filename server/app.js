5a493f75beeeffe5d391ac5dfc04287349237c65
mateuszrosa <mateusz.rosa@gmail.com>
2021-10-04 13:56:46 +0200

    remove bodyparser from dependencies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 export const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.use(cors());
 
